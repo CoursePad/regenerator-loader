e799480906998c2f3106c9b75e95d857022e3309
Jingsi Zhu <zhu@jing.si>
2014-12-05 00:12:53 +0000

    Bump regenerator version

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var regenerator = require('regenerator');
 
 module.exports = function (source, map) {
     this.cacheable && this.cacheable();
-    var result = regenerator(source);
-    if (result !== source) {
-        // Ensure that regenerator runtime is included and initialized,
-        // and drop source map, since Regenerator doesn't support it (yet)       
-        return "require('regenerator/runtime/min');\n"+result;
+    var result;
+
+    if (source.indexOf('yield') > -1 || source.indexOf('function*') > -1 || source.indexOf('async') > -1 || source.indexOf('await') > -1) {
+        result = regenerator.compile(source).code;
+        if (result !== source) {
+            // Ensure that regenerator runtime is included and initialized,
+            // and drop source map, since Regenerator doesn't support it (yet)       
+            return "require('regenerator/runtime');\n"+result;
+        }
+    } else {
+        result = source;
     }
+
+    
     // No change; pass through original source and map, if any, via async mode
     this.async().apply(
         this, [null].concat(Array.prototype.slice.call(arguments))
